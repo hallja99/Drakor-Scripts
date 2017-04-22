8748e8f6e2983c30ec58fc3bf0c77b77479d9f54
Jake Hall <hallja99@gmail.com>
2017-04-21 23:16:32 -0400

    Tamper Monkey Compatible
    -Script is now usable with Tamper Monkey
    -time reduced on menu delay

--- Online TH Nodes.js
+++ Online TH Nodes.js
@@ -1,45 +1,55 @@
-var button = document.createElement("button")
-button.innerHTML = "Find TH Nodes"
-button.class = "gs_topmenu_item"
-document.getElementById("gs_topmenu").appendChild(button)
+// ==UserScript==
+// @name         OnlineTHNodes
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Prints active TH nodes in the online list to console
+// @author       Oveduumnakal
+// @match        http://*.drakor.com*
+// @match        https://*.drakor.com*
+// ==/UserScript==
+
+var button = document.createElement("button");
+button.innerHTML = "Find TH Nodes";
+button.classList.add('gs_topmenu_item');
+document.getElementById("gs_topmenu").appendChild(button);
 
 button.addEventListener("click", function(){
-	document.getElementById("s_online").click()
-	var thNodes = []
+	document.getElementById("s_online").click();
+	var thNodes = [];
 
 	setTimeout(function(){
 		function getTH(list){
 			for(var i = 0; i < list.length; i++){
-				var tmp = list[i].children[2].innerText
+				var tmp = list[i].children[2].innerText;
 				if(tmp.toLowerCase().indexOf("treasure") != -1){
-					thNodes.push(tmp.split("@")[1].trim())
+					thNodes.push(tmp.split("@")[1].trim());
 				}
 			}
 		}
 
-		var online = document.getElementById("s_online_details").children[1]
+		var online = document.getElementById("s_online_details").children[1];
 
-		document.getElementById("s_online").click()
+		document.getElementById("s_online").click();
 		
-		var x = online.children[1].children
-		var y = online.children[3].children
-		var z = online.children[5].children
+		var x = online.children[1].children;
+		var y = online.children[3].children;
+		var z = online.children[5].children;
 
-		getTH(x)
-		getTH(y)
-		getTH(z)
+		getTH(x);
+		getTH(y);
+		getTH(z);
 
 		for(var i = 0; i < thNodes.length; i++){
 			for(var j = i + 1; j <thNodes.length; j++){
 				if( j < thNodes.length + 1 && thNodes[i] == thNodes[j]){
-					thNodes.splice(j, 1)
-					j--
+					thNodes.splice(j, 1);
+					j--;
 				}
 			}
 		}
 
-		for(var i = 0; i < thNodes.length; i++){
-			console.log("TH Node at: " + thNodes[i])
+		for(i = 0; i < thNodes.length; i++){
+			console.log("TH Node at: " + thNodes[i]);
 		}
-	},1000)
-})
+	},450);
+});
